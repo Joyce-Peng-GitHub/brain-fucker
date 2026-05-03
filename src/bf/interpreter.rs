550a37d40e04e0665004f2ff6ce4acfa33a18d39
Joyce <1071060893@qq.com>
2026-05-03 23:47:04 +0800

    Refactor `Interpreter` to store codes in a vector of `Instruction` enum.

--- src/bf/interpreter.rs
+++ src/bf/interpreter.rs
@@ -1,21 +1,21 @@
 use std::{
+    any::Any,
     io::{Read, Write},
     usize,
 };
 
 use crate::bf::executor::Executor;
 
-/// (character, number of occurrences or index of the matching bracket)
-struct Code(u8, usize);
-
-pub struct Interpreter<R, W> {
-    executor: Executor<R, W>,
-    codes: Vec<Code>,
-    instr_ptr: usize,
+enum Instruction {
+    MoveDataPtr(isize),
+    WrappingAddCurByte(u8),
+    WriteByte,
+    ReadByte,
+    JmpFwd(usize),
+    JmpBwd(usize),
 }
 
-impl<R: Read, W: Write> Interpreter<R, W> {
-    const VALID_CHARS: &'static [u8] = b"><+-.,[]";
+impl Instruction {
     const INC_DATA_PTR: u8 = b'>';
     const DEC_DATA_PTR: u8 = b'<';
     const INC_CUR_BYTE: u8 = b'+';
@@ -25,40 +25,92 @@ impl<R: Read, W: Write> Interpreter<R, W> {
     const JMP_FWD: u8 = b'[';
     const JMP_BWD: u8 = b']';
 
-    fn parse_code(code: &Vec<u8>) -> Result<Vec<Code>, String> {
-        let mut codes = Vec::<Code>::with_capacity(code.len());
-
-        for &b in code.iter() {
-            if !Self::VALID_CHARS.contains(&b) {
-                continue;
-            }
-
-            if !codes.is_empty()
-                && codes.last().unwrap().0 == b
-                && b != Self::JMP_FWD
-                && b != Self::JMP_BWD
-            {
-                codes.last_mut().unwrap().1 += 1;
-            } else {
-                codes.push(Code(b, 1));
-            }
+    fn from_byte(b: u8) -> Option<Self> {
+        match b {
+            Self::INC_DATA_PTR => Some(Self::MoveDataPtr(1)),
+            Self::DEC_DATA_PTR => Some(Self::MoveDataPtr(-1)),
+            Self::INC_CUR_BYTE => Some(Self::WrappingAddCurByte(1)),
+            Self::DEC_CUR_BYTE => Some(Self::WrappingAddCurByte(u8::MAX)), // -1 mod 256
+            Self::WRITE_BYTE => Some(Self::WriteByte),
+            Self::READ_BYTE => Some(Self::ReadByte),
+            Self::JMP_FWD => Some(Self::JmpFwd(usize::MAX)), // Placeholder, will be set in `parse_codes`
+            Self::JMP_BWD => Some(Self::JmpBwd(usize::MAX)), // Placeholder, will be set in `parse_codes`
+            _ => None,
         }
+    }
+}
 
+pub struct Interpreter<R, W> {
+    executor: Executor<R, W>,
+    instructions: Vec<Instruction>,
+    instr_ptr: usize,
+}
+
+impl<R: Read, W: Write> Interpreter<R, W> {
+    fn compress_codes(codes: &Vec<u8>) -> Result<Vec<Instruction>, String> {
+        let mut instructions = Vec::<Instruction>::with_capacity(codes.len());
         let mut stk = Vec::<usize>::with_capacity(codes.len());
-        for i in 0..codes.len() {
-            match codes[i].0 {
-                Self::JMP_FWD => stk.push(i),
-                Self::JMP_BWD => {
-                    if let Some(open_idx) = stk.pop() {
-                        codes[open_idx].1 = i;
-                        codes[i].1 = open_idx;
+
+        for &b in codes.iter() {
+            if let Some(instr) = Instruction::from_byte(b) {
+                if let Instruction::JmpFwd(pos) = instr {
+                    debug_assert_eq!(pos, usize::MAX);
+                    stk.push(instructions.len());
+                    instructions.push(instr);
+                    continue;
+                } else if let Instruction::JmpBwd(pos) = instr {
+                    debug_assert_eq!(pos, usize::MAX);
+                    if let Some(open_pos) = stk.pop() {
+                        let close_pos = instructions.len();
+                        if let Instruction::JmpFwd(ref mut open_match_pos) = instructions[open_pos]
+                        {
+                            *open_match_pos = close_pos;
+                        } else {
+                            unreachable!();
+                        }
+                        instructions.push(Instruction::JmpBwd(open_pos));
                     } else {
-                        return Err(format!("Unmatched closing bracket at position {}", i));
+                        return Err(format!(
+                            "Unmatched closing bracket at position {}",
+                            instructions.len()
+                        ));
                     }
+                    continue;
                 }
-                _ => {}
+
+                if instructions.is_empty()
+                    || matches!(instr, Instruction::WriteByte | Instruction::ReadByte)
+                    || std::mem::discriminant(instructions.last().unwrap())
+                        != std::mem::discriminant(&instr)
+                {
+                    instructions.push(instr);
+                    continue;
+                }
+
+                match instr {
+                    Instruction::MoveDataPtr(offset) => {
+                        if let Instruction::MoveDataPtr(cur_offset) =
+                            instructions.last_mut().unwrap()
+                        {
+                            *cur_offset += offset;
+                        } else {
+                            unreachable!(); // The `if` condition above guarantees this won't happen
+                        }
+                    }
+                    Instruction::WrappingAddCurByte(diff) => {
+                        if let Instruction::WrappingAddCurByte(cur_diff) =
+                            instructions.last_mut().unwrap()
+                        {
+                            *cur_diff = cur_diff.wrapping_add(diff);
+                        } else {
+                            unreachable!(); // The `if` condition above guarantees this won't happen
+                        }
+                    }
+                    _ => unreachable!(),
+                }
             }
         }
+
         if !stk.is_empty() {
             return Err(format!(
                 "Unmatched opening bracket at position {}",
@@ -66,58 +118,43 @@ impl<R: Read, W: Write> Interpreter<R, W> {
             ));
         }
 
-        Ok(codes)
+        Ok(instructions)
     }
 
-    pub fn try_new(executor: Executor<R, W>, code: Vec<u8>) -> Result<Self, String> {
+    pub fn try_new(executor: Executor<R, W>, codes: Vec<u8>) -> Result<Self, String> {
         let interpreter = Interpreter {
             executor: executor,
-            codes: Self::parse_code(&code)?,
+            instructions: Self::compress_codes(&codes)?,
             instr_ptr: 0,
         };
         Ok(interpreter)
     }
 
     pub fn exec_once(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        if self.instr_ptr >= self.codes.len() {
+        if self.instr_ptr >= self.instructions.len() {
             return Err(format!(
                 "Instruction pointer out of bounds: {} >= {}",
                 self.instr_ptr,
-                self.codes.len()
+                self.instructions.len()
             )
             .into());
         }
 
-        match self.codes[self.instr_ptr] {
-            Code(Self::INC_DATA_PTR, cnt) => self.executor.move_data_ptr(cnt as isize)?,
-            Code(Self::DEC_DATA_PTR, cnt) => self.executor.move_data_ptr(-(cnt as isize))?,
-            Code(Self::INC_CUR_BYTE, cnt) => {
-                self.executor.add_cur_byte((cnt & (u8::MAX as usize)) as u8)
-            }
-            Code(Self::DEC_CUR_BYTE, cnt) => {
-                self.executor.sub_cur_byte((cnt & (u8::MAX as usize)) as u8)
-            }
-            Code(Self::WRITE_BYTE, cnt) => {
-                for _ in 0..cnt {
-                    self.executor.write_byte()?;
-                }
-            }
-            Code(Self::READ_BYTE, cnt) => {
-                for _ in 0..cnt {
-                    self.executor.read_byte()?;
-                }
-            }
-            Code(Self::JMP_FWD, match_idx) => {
+        match self.instructions[self.instr_ptr] {
+            Instruction::MoveDataPtr(offset) => self.executor.move_data_ptr(offset)?,
+            Instruction::WrappingAddCurByte(diff) => self.executor.add_cur_byte(diff),
+            Instruction::WriteByte => self.executor.write_byte()?,
+            Instruction::ReadByte => self.executor.read_byte()?,
+            Instruction::JmpFwd(pos) => {
                 if self.executor.cur_byte() == 0 {
-                    self.instr_ptr = match_idx;
+                    self.instr_ptr = pos;
                 }
             }
-            Code(Self::JMP_BWD, match_idx) => {
+            Instruction::JmpBwd(pos) => {
                 if self.executor.cur_byte() != 0 {
-                    self.instr_ptr = match_idx;
+                    self.instr_ptr = pos;
                 }
             }
-            _ => unreachable!(), // `parse_code` guarantees this won't happen
         }
 
         self.instr_ptr += 1;
@@ -125,7 +162,7 @@ impl<R: Read, W: Write> Interpreter<R, W> {
     }
 
     pub fn exec(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        while self.instr_ptr < self.codes.len() {
+        while self.instr_ptr < self.instructions.len() {
             self.exec_once()?;
         }
         Ok(())
